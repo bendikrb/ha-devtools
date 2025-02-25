7adc6ec855ed2872ef814de7c0d51a0f4cd2b7b1
Bendik R. Brenne <bendik@konstant.no>
2025-02-26 00:05:06 +0100

    Add support for custom HA URL and auto token generation

--- custom_components/devtools/__init__.py
+++ custom_components/devtools/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_TOKEN
+from homeassistant.const import CONF_TOKEN, CONF_URL
 from homeassistant.core import (
     HomeAssistant,
     ServiceCall,
@@ -9,7 +9,6 @@ from homeassistant.core import (
     SupportsResponse,
 )
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.network import get_url
 
 from .const import DOMAIN
 from .ha_connection import HAConnection
@@ -21,6 +20,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up devtools from a config entry."""
 
     access_token = entry.data[CONF_TOKEN]
+    url = entry.data[CONF_URL]
     web_session = async_get_clientsession(hass)
 
     async def handle_call_ws_endpoint(call: ServiceCall) -> ServiceResponse:
@@ -29,7 +29,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
         message = {"type": ws_type, **ws_data}
         async with HAConnection(
-            url=get_url(hass),
+            url=url,
             token=access_token,
             hass=hass,
             session=web_session,
